bd8bd186098dcf25237e693d74769a422ab6cefd
INT\xhuae08006 <xiaotian.huang1@elekta.com>
2021-01-04 15:19:50 +0800

    Please note dose should be transfered into 2Gy/fraction firstly.

--- CF_Display.py
+++ CF_Display.py
@@ -25,3 +25,25 @@ plt.xlabel("Dose(Gy)")
 plt.ylabel("Volume(%)")
 plt.grid()
 plt.show()
+
+
+
+# Parallel function
+D_0 = 30   # unit:Gy
+K   = 15   # Power Law
+X = np.arange(0,Sample_DVH.shape[0]/100,0.01)
+MM = 1/(1+(D_0/X)**K)
+MM_1 = MM/np.max(MM)*100
+KK = 5*(MM_1[1:] - MM_1[0:-1])/0.01
+
+f = pd.DataFrame(KK)
+Sample_DVH_Plot = pd.concat([Sample_DVH_Plot,f],axis=1)
+Sample_DVH_Plot.columns = ['HNSCC-01-0002_rtss','K=5&D_0=25Gy','K=10&D_0=25Gy','K=1&D_0=25Gy','K=5&D_0=30Gy','K=15&D_0=30Gy']
+plt.figure()
+Sample_DVH_Plot.plot()
+
+plt.xlabel("Dose(Gy)")
+plt.ylabel("Volume(%)")
+plt.grid()
+plt.savefig("C:\\Users\\xhuae08006\\OneDrive - Elekta\\Desktop\\CFs_display.png",dpi=200, bbox_inches='tight')
+plt.show()
