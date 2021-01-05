293500ade666fdffcf83b9dc126b971afbedee43
INT\xhuae08006 <xiaotian.huang1@elekta.com>
2021-01-05 17:19:41 +0800

    add the first app in this project

---
+++ GUI_Streamlit.py
@@ -0,0 +1,15 @@
+import streamlit as st
+# To make things easier later, we're also importing numpy and pandas for
+# working with sample data.
+import numpy as np
+import pandas as pd
+
+st.title('My first app')
+
+## Logic for the project
+# 1) upload users' own e-protocols
+# 2) provide tg report 
+
+
+
+
