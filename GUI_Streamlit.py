661c15b4d43850c86ca21445e571b91cee79d216
INT\xhuae08006 <xiaotian.huang1@elekta.com>
2021-01-06 10:20:16 +0800

    add ui streamlit app codes

--- GUI_Streamlit.py
+++ GUI_Streamlit.py
@@ -8,7 +8,9 @@ st.title('My first app')
 
 ## Logic for the project
 # 1) upload users' own e-protocols
-# 2) provide tg report 
+# 2) provide tg report as a reference for rechecking
+# 3) machine learning module for prediction of DVH
+# 4) Monaco Template generation each version, download as a zip folder
 
 
 
