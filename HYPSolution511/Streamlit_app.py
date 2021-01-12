455d6fb9996cf90a90663a7ecbb05d6846c0f0aa
INT\xhuae08006 <xiaotian.huang1@elekta.com>
2021-01-12 11:27:40 +0800

    change selectbox to radio in UI

--- HYPSolution511/Streamlit_app.py
+++ HYPSolution511/Streamlit_app.py
@@ -8,13 +8,15 @@ from MONV559b import MONV559b_UI
 from MONACO511_MPTD_WEB import MONV511_UI_MPTD
 
 st.sidebar.header('Monaco Plan Template Generation V1.0')
-mon_option1 = st.sidebar.selectbox(
-    'Monaco Versions Selection',
-    ('MONV511','MONV559b'))
+# mon_option1 = st.sidebar.selectbox(
+#     'Monaco Versions Selection',
+#     ('MONV511','MONV559b'))
 
-Mode = st.sidebar.selectbox(
-    'Pareto or Constrained Mode Selection',
-    ('Constrained','Pareto'))
+mon_option1 = st.sidebar.radio('Monaco Versions Selection', 
+                              ('MONV511','MONV559b'))
+
+Mode = st.sidebar.radio('Pareto or Constrained Mode Selection',
+                       ('Constrained','Pareto'))
 
 if mon_option1 == 'MONV511':
     MONV511_UI(Mode)
@@ -23,20 +25,20 @@ elif mon_option1 == 'MONV559b':
 
 st.sidebar.header('Monaco Plan Template Decode V1.0')
 
-mon_option2 = st.sidebar.selectbox(
+mon_option2 = st.sidebar.radio(
     'Monaco Selection',
     ('MONV511','MONV559b'))
 
 st.sidebar.header('Monaco Plan Template Management V1.0')
 
-mon_option3 = st.sidebar.selectbox(
+mon_option3 = st.sidebar.radio(
     'Monaco Version',
     ('MONV511','MONV559b'))
 
 
 st.sidebar.header('Monaco Plan Template Upgrade V1.0 ')
 
-mon_option4 = st.sidebar.selectbox(
+mon_option4 = st.sidebar.radio(
     'Monaco TPS Version',
     ('MONV511','MONV559b'))
 
